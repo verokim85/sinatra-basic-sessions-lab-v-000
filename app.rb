8a68e42f849531edf3fdf63aca03230225b4018f
Veronica Kim <vero.a.kim@gmail.com>
2018-05-27 02:18:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    @session = session
+    
+  end
+
+  # post '/checkout' do
+  #   @sessions = params([:item])
+  # end
+
 end
